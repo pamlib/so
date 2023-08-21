d6539edfe520f5f2adca6eae6fee04720095f589
pamlib <82670434+pamlib@users.noreply.github.com>
2023-08-21 14:44:59 -0400

    Update episodes.tsx

--- app/routes/anime+/$animeId+/episodes.tsx
+++ app/routes/anime+/$animeId+/episodes.tsx
@@ -50,7 +50,7 @@ export const meta = mergeMeta<{}, { 'routes/anime+/$animeId': typeof animeIdLoad
       },
       {
         property: 'og:url',
-        content: `https://sorachill.vercel.app/anime/${params.animeId}/episodes`,
+        content: `https://so-tau.vercel.app/anime/${params.animeId}/episodes`,
       },
       { property: 'og:title', content: `Sora - ${animeTitle} - Episodes` },
       {
